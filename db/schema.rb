432a4821a50d6f5a4626d55dc259666f1b0f42c1
umair ali <umair.ali.aldaim@gmail.com>
2017-10-27 12:43:06 +0500

    yml change

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171026082657) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
